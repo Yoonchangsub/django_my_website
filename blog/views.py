d1379e07b5c755d7c6b98df2657165e72e8e478d
Yoon ChangSub <yoonchs01@gmail.com>
2020-07-14 23:40:47 +0900

    postlist View

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,7 @@ class PostList(ListView):
     def get_queryset(self):
         return Post.objects.order_by('created')
 
+
 # def index(request):
 #     posts = Post.objects.all()
     # return render(
